e49dfe0945fbb52dd0b1abae826e85d4f89333f6
MikeNotu <michael.bateman@globant.com>
2022-09-20 17:42:51 -0500

    Testing Typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Sum } from "./services/Sum";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button
+        onClick={() => {
+          alert(Sum(3, 5));
+        }}
+      >
+        Suma de 3 y 5
+      </button>
     </div>
   );
 }
