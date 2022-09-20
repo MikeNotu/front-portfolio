36af1ec4ee491685be3ceedde33b3b92812d4e25
MikeNotu <michael.bateman@globant.com>
2022-09-20 18:08:21 -0500

    Material UI Tested and it seems to be working

--- src/components/MyName/MyName.tsx
+++ src/components/MyName/MyName.tsx
@@ -1,7 +1,15 @@
 import "../MyName/MyName.scss";
+import Avatar from "@mui/material/Avatar";
+import { deepPurple } from "@mui/material/colors";
+
 function MyName() {
   return (
     <div>
+      <Avatar
+        alt="Hello"
+        src="https://media.istockphoto.com/vectors/thumb-up-emoticon-vector-id157030584?k=20&m=157030584&s=612x612&w=0&h=TsRUZrgOW19D1f3WMleDrgGL-xfI6g0ZYhJDp58lS0E="
+      />
+      <Avatar sx={{ bgcolor: deepPurple[500] }}>OP</Avatar>
       <h1 className="blue">Test</h1>
     </div>
   );
