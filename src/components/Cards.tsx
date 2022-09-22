8c419f7e146132902b591575f17f828df788df8c
MikeNotu <michael.bateman@globant.com>
2022-09-22 13:05:07 -0500

    Each Card dynamically changes when pressed

--- src/components/Cards.tsx
+++ src/components/Cards.tsx
@@ -31,20 +31,31 @@ const ExpandMore = styled((props: ExpandMoreProps) => {
   }),
 }));
 
+const DATA = [
+  {
+    title: "Titulo",
+    texto: "Lorem Ipsum",
+    date: "September 14, 2016",
+    description: "Loremloremlorem",
+  },
+];
+
 export default function RecipeReviewCard() {
   const [expanded, setExpanded] = React.useState(false);
+  const [upexpanded, setupExpanded] = React.useState([false, false]);
 
   const handleExpandClick = () => {
-    setExpanded(!expanded);
+    // setExpanded(!expanded);
+    setupExpanded([!upexpanded[0], upexpanded[1]]);
   };
 
   return (
     <Grid alignItems="center" justifyContent="center" container margin={1}>
-      <Card sx={{ maxWidth: 345, margin: 3 }}>
+      <Card sx={{ maxWidth: 345, margin: 3, mb: "auto" }}>
         <CardHeader
           avatar={
             <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
-              R
+              M
             </Avatar>
           }
           action={
@@ -52,8 +63,8 @@ export default function RecipeReviewCard() {
               <MoreVertIcon />
             </IconButton>
           }
-          title="Shrimp and Chorizo Paella"
-          subheader="September 14, 2016"
+          title={DATA[0].title}
+          subheader={DATA[0].date}
         />
         <CardMedia
           component="img"
@@ -63,9 +74,7 @@ export default function RecipeReviewCard() {
         />
         <CardContent>
           <Typography variant="body2" color="text.secondary">
-            This impressive paella is a perfect party dish and a fun meal to
-            cook together with your guests. Add 1 cup of frozen peas along with
-            the mussels, if you like.
+            {DATA[0].texto}
           </Typography>
         </CardContent>
         <CardActions disableSpacing>
@@ -76,7 +85,7 @@ export default function RecipeReviewCard() {
             <ShareIcon />
           </IconButton>
           <ExpandMore
-            expand={expanded}
+            expand={upexpanded[0]}
             onClick={handleExpandClick}
             aria-expanded={expanded}
             aria-label="show more"
@@ -84,7 +93,7 @@ export default function RecipeReviewCard() {
             <ExpandMoreIcon />
           </ExpandMore>
         </CardActions>
-        <Collapse in={expanded} timeout="auto" unmountOnExit>
+        <Collapse in={upexpanded[0]} timeout="auto" unmountOnExit>
           <CardContent>
             <Typography paragraph>Method:</Typography>
             <Typography paragraph>
@@ -117,11 +126,11 @@ export default function RecipeReviewCard() {
           </CardContent>
         </Collapse>
       </Card>
-      <Card sx={{ maxWidth: 345, margin: 3 }}>
+      <Card sx={{ maxWidth: 345, margin: 3, mb: "auto" }}>
         <CardHeader
           avatar={
             <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
-              R
+              M
             </Avatar>
           }
           action={
@@ -129,8 +138,8 @@ export default function RecipeReviewCard() {
               <MoreVertIcon />
             </IconButton>
           }
-          title="Shrimp and Chorizo Paella"
-          subheader="September 14, 2016"
+          title={DATA[0].title}
+          subheader={DATA[0].date}
         />
         <CardMedia
           component="img"
@@ -140,9 +149,7 @@ export default function RecipeReviewCard() {
         />
         <CardContent>
           <Typography variant="body2" color="text.secondary">
-            This impressive paella is a perfect party dish and a fun meal to
-            cook together with your guests. Add 1 cup of frozen peas along with
-            the mussels, if you like.
+            {DATA[0].texto}
           </Typography>
         </CardContent>
         <CardActions disableSpacing>
@@ -153,7 +160,7 @@ export default function RecipeReviewCard() {
             <ShareIcon />
           </IconButton>
           <ExpandMore
-            expand={expanded}
+            expand={upexpanded[1]}
             onClick={handleExpandClick}
             aria-expanded={expanded}
             aria-label="show more"
@@ -161,7 +168,7 @@ export default function RecipeReviewCard() {
             <ExpandMoreIcon />
           </ExpandMore>
         </CardActions>
-        <Collapse in={expanded} timeout="auto" unmountOnExit>
+        <Collapse in={upexpanded[1]} timeout="auto" unmountOnExit>
           <CardContent>
             <Typography paragraph>Method:</Typography>
             <Typography paragraph>
@@ -194,11 +201,11 @@ export default function RecipeReviewCard() {
           </CardContent>
         </Collapse>
       </Card>
-      <Card sx={{ maxWidth: 345, margin: 3 }}>
+      <Card sx={{ maxWidth: 345, margin: 3, mb: "auto" }}>
         <CardHeader
           avatar={
             <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
-              R
+              M
             </Avatar>
           }
           action={
@@ -206,8 +213,8 @@ export default function RecipeReviewCard() {
               <MoreVertIcon />
             </IconButton>
           }
-          title="Shrimp and Chorizo Paella"
-          subheader="September 14, 2016"
+          title={DATA[0].title}
+          subheader={DATA[0].date}
         />
         <CardMedia
           component="img"
@@ -217,9 +224,7 @@ export default function RecipeReviewCard() {
         />
         <CardContent>
           <Typography variant="body2" color="text.secondary">
-            This impressive paella is a perfect party dish and a fun meal to
-            cook together with your guests. Add 1 cup of frozen peas along with
-            the mussels, if you like.
+            {DATA[0].texto}
           </Typography>
         </CardContent>
         <CardActions disableSpacing>
@@ -230,7 +235,7 @@ export default function RecipeReviewCard() {
             <ShareIcon />
           </IconButton>
           <ExpandMore
-            expand={expanded}
+            expand={upexpanded[1]}
             onClick={handleExpandClick}
             aria-expanded={expanded}
             aria-label="show more"
@@ -238,7 +243,7 @@ export default function RecipeReviewCard() {
             <ExpandMoreIcon />
           </ExpandMore>
         </CardActions>
-        <Collapse in={expanded} timeout="auto" unmountOnExit>
+        <Collapse in={upexpanded[1]} timeout="auto" unmountOnExit>
           <CardContent>
             <Typography paragraph>Method:</Typography>
             <Typography paragraph>
@@ -271,11 +276,11 @@ export default function RecipeReviewCard() {
           </CardContent>
         </Collapse>
       </Card>
-      <Card sx={{ maxWidth: 345, margin: 3 }}>
+      <Card sx={{ maxWidth: 345, margin: 3, mb: "auto" }}>
         <CardHeader
           avatar={
             <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
-              R
+              M
             </Avatar>
           }
           action={
@@ -283,8 +288,8 @@ export default function RecipeReviewCard() {
               <MoreVertIcon />
             </IconButton>
           }
-          title="Shrimp and Chorizo Paella"
-          subheader="September 14, 2016"
+          title={DATA[0].title}
+          subheader={DATA[0].date}
         />
         <CardMedia
           component="img"
@@ -294,9 +299,7 @@ export default function RecipeReviewCard() {
         />
         <CardContent>
           <Typography variant="body2" color="text.secondary">
-            This impressive paella is a perfect party dish and a fun meal to
-            cook together with your guests. Add 1 cup of frozen peas along with
-            the mussels, if you like.
+            {DATA[0].texto}
           </Typography>
         </CardContent>
         <CardActions disableSpacing>
@@ -307,7 +310,7 @@ export default function RecipeReviewCard() {
             <ShareIcon />
           </IconButton>
           <ExpandMore
-            expand={expanded}
+            expand={upexpanded[1]}
             onClick={handleExpandClick}
             aria-expanded={expanded}
             aria-label="show more"
@@ -315,7 +318,7 @@ export default function RecipeReviewCard() {
             <ExpandMoreIcon />
           </ExpandMore>
         </CardActions>
-        <Collapse in={expanded} timeout="auto" unmountOnExit>
+        <Collapse in={upexpanded[1]} timeout="auto" unmountOnExit>
           <CardContent>
             <Typography paragraph>Method:</Typography>
             <Typography paragraph>
@@ -348,11 +351,11 @@ export default function RecipeReviewCard() {
           </CardContent>
         </Collapse>
       </Card>
-      <Card sx={{ maxWidth: 345, margin: 3 }}>
+      <Card sx={{ maxWidth: 345, margin: 3, mb: "auto" }}>
         <CardHeader
           avatar={
             <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
-              R
+              M
             </Avatar>
           }
           action={
@@ -360,8 +363,8 @@ export default function RecipeReviewCard() {
               <MoreVertIcon />
             </IconButton>
           }
-          title="Shrimp and Chorizo Paella"
-          subheader="September 14, 2016"
+          title={DATA[0].title}
+          subheader={DATA[0].date}
         />
         <CardMedia
           component="img"
@@ -371,9 +374,7 @@ export default function RecipeReviewCard() {
         />
         <CardContent>
           <Typography variant="body2" color="text.secondary">
-            This impressive paella is a perfect party dish and a fun meal to
-            cook together with your guests. Add 1 cup of frozen peas along with
-            the mussels, if you like.
+            {DATA[0].texto}
           </Typography>
         </CardContent>
         <CardActions disableSpacing>
@@ -384,7 +385,7 @@ export default function RecipeReviewCard() {
             <ShareIcon />
           </IconButton>
           <ExpandMore
-            expand={expanded}
+            expand={upexpanded[1]}
             onClick={handleExpandClick}
             aria-expanded={expanded}
             aria-label="show more"
@@ -392,7 +393,7 @@ export default function RecipeReviewCard() {
             <ExpandMoreIcon />
           </ExpandMore>
         </CardActions>
-        <Collapse in={expanded} timeout="auto" unmountOnExit>
+        <Collapse in={upexpanded[1]} timeout="auto" unmountOnExit>
           <CardContent>
             <Typography paragraph>Method:</Typography>
             <Typography paragraph>
@@ -425,11 +426,11 @@ export default function RecipeReviewCard() {
           </CardContent>
         </Collapse>
       </Card>
-      <Card sx={{ maxWidth: 345, margin: 3 }}>
+      <Card sx={{ maxWidth: 345, margin: 3, mb: "auto" }}>
         <CardHeader
           avatar={
             <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
-              R
+              M
             </Avatar>
           }
           action={
@@ -437,8 +438,8 @@ export default function RecipeReviewCard() {
               <MoreVertIcon />
             </IconButton>
           }
-          title="Shrimp and Chorizo Paella"
-          subheader="September 14, 2016"
+          title={DATA[0].title}
+          subheader={DATA[0].date}
         />
         <CardMedia
           component="img"
@@ -448,9 +449,7 @@ export default function RecipeReviewCard() {
         />
         <CardContent>
           <Typography variant="body2" color="text.secondary">
-            This impressive paella is a perfect party dish and a fun meal to
-            cook together with your guests. Add 1 cup of frozen peas along with
-            the mussels, if you like.
+            {DATA[0].texto}
           </Typography>
         </CardContent>
         <CardActions disableSpacing>
@@ -461,7 +460,7 @@ export default function RecipeReviewCard() {
             <ShareIcon />
           </IconButton>
           <ExpandMore
-            expand={expanded}
+            expand={upexpanded[1]}
             onClick={handleExpandClick}
             aria-expanded={expanded}
             aria-label="show more"
@@ -469,7 +468,82 @@ export default function RecipeReviewCard() {
             <ExpandMoreIcon />
           </ExpandMore>
         </CardActions>
-        <Collapse in={expanded} timeout="auto" unmountOnExit>
+        <Collapse in={upexpanded[1]} timeout="auto" unmountOnExit>
+          <CardContent>
+            <Typography paragraph>Method:</Typography>
+            <Typography paragraph>
+              Heat 1/2 cup of the broth in a pot until simmering, add saffron
+              and set aside for 10 minutes.
+            </Typography>
+            <Typography paragraph>
+              Heat oil in a (14- to 16-inch) paella pan or a large, deep skillet
+              over medium-high heat. Add chicken, shrimp and chorizo, and cook,
+              stirring occasionally until lightly browned, 6 to 8 minutes.
+              Transfer shrimp to a large plate and set aside, leaving chicken
+              and chorizo in the pan. Add pimentón, bay leaves, garlic,
+              tomatoes, onion, salt and pepper, and cook, stirring often until
+              thickened and fragrant, about 10 minutes. Add saffron broth and
+              remaining 4 1/2 cups chicken broth; bring to a boil.
+            </Typography>
+            <Typography paragraph>
+              Add rice and stir very gently to distribute. Top with artichokes
+              and peppers, and cook without stirring, until most of the liquid
+              is absorbed, 15 to 18 minutes. Reduce heat to medium-low, add
+              reserved shrimp and mussels, tucking them down into the rice, and
+              cook again without stirring, until mussels have opened and rice is
+              just tender, 5 to 7 minutes more. (Discard any mussels that
+              don&apos;t open.)
+            </Typography>
+            <Typography>
+              Set aside off of the heat to let rest for 10 minutes, and then
+              serve.
+            </Typography>
+          </CardContent>
+        </Collapse>
+      </Card>
+      <Card sx={{ maxWidth: 345, margin: 3, mb: "auto" }}>
+        <CardHeader
+          avatar={
+            <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
+              M
+            </Avatar>
+          }
+          action={
+            <IconButton aria-label="settings">
+              <MoreVertIcon />
+            </IconButton>
+          }
+          title={DATA[0].title}
+          subheader={DATA[0].date}
+        />
+        <CardMedia
+          component="img"
+          height="194"
+          image="https://i.ytimg.com/vi/XY7TDNVvHqs/maxresdefault.jpg"
+          alt="Paella dish"
+        />
+        <CardContent>
+          <Typography variant="body2" color="text.secondary">
+            {DATA[0].texto}
+          </Typography>
+        </CardContent>
+        <CardActions disableSpacing>
+          <IconButton aria-label="add to favorites">
+            <FavoriteIcon />
+          </IconButton>
+          <IconButton aria-label="share">
+            <ShareIcon />
+          </IconButton>
+          <ExpandMore
+            expand={upexpanded[1]}
+            onClick={handleExpandClick}
+            aria-expanded={expanded}
+            aria-label="show more"
+          >
+            <ExpandMoreIcon />
+          </ExpandMore>
+        </CardActions>
+        <Collapse in={upexpanded[1]} timeout="auto" unmountOnExit>
           <CardContent>
             <Typography paragraph>Method:</Typography>
             <Typography paragraph>
