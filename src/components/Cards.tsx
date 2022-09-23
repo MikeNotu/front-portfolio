d0f8cd3f257359bc55a8704b0ae9f8ff6905ea5d
MikeNotu <michael.bateman@globant.com>
2022-09-23 16:43:35 -0500

    First cards finished

--- src/components/Cards.tsx
+++ src/components/Cards.tsx
@@ -48,15 +48,16 @@ interface Data {
 
 const DATA: Data[] = [
   {
-    title: "Internal Project",
+    title: "Technologies Project",
     text: "React, Redux, Typescript, React Testing Library + Jest, React Router, HOCs & SCSS",
     date: "Dec 30, 2021",
-    description: "Loremloremlorem",
+    description:
+      "Project created to demonstrate implementation of multiple technologies working together.",
     url: "  https://codesandbox.io/s/bateman-redux-typescript-t3w0i",
     image: Img_ReduxTestingHocs,
   },
   {
-    title: "Titulo",
+    title: "Chess Champions",
     text: "React, Javascript, Fetch, Material UI(MUI), React Router & SCSS",
     date: "Jul 11, 2021",
     description: "Loremloremlorem",
@@ -180,35 +181,7 @@ const Cards: React.FC = () => {
             </CardActions>
             <Collapse in={expanded[index]} timeout="auto" unmountOnExit>
               <CardContent>
-                <Typography paragraph>Method:</Typography>
-                <Typography paragraph>
-                  Heat 1/2 cup of the broth in a pot until simmering, add
-                  saffron and set aside for 10 minutes.
-                </Typography>
-                <Typography paragraph>
-                  Heat oil in a (14- to 16-inch) paella pan or a large, deep
-                  skillet over medium-high heat. Add chicken, shrimp and
-                  chorizo, and cook, stirring occasionally until lightly
-                  browned, 6 to 8 minutes. Transfer shrimp to a large plate and
-                  set aside, leaving chicken and chorizo in the pan. Add
-                  pimentón, bay leaves, garlic, tomatoes, onion, salt and
-                  pepper, and cook, stirring often until thickened and fragrant,
-                  about 10 minutes. Add saffron broth and remaining 4 1/2 cups
-                  chicken broth; bring to a boil.
-                </Typography>
-                <Typography paragraph>
-                  Add rice and stir very gently to distribute. Top with
-                  artichokes and peppers, and cook without stirring, until most
-                  of the liquid is absorbed, 15 to 18 minutes. Reduce heat to
-                  medium-low, add reserved shrimp and mussels, tucking them down
-                  into the rice, and cook again without stirring, until mussels
-                  have opened and rice is just tender, 5 to 7 minutes more.
-                  (Discard any mussels that don&apos;t open.)
-                </Typography>
-                <Typography>
-                  Set aside off of the heat to let rest for 10 minutes, and then
-                  serve.
-                </Typography>
+                <Typography>{data.description}</Typography>
               </CardContent>
             </Collapse>
           </Card>
