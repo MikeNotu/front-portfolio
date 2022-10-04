4a0955d4e269501e192ef83b12875f81d4494f19
MikeNotu <michael.bateman@globant.com>
2022-10-04 16:57:29 -0500

    Bubbles ReWork, Modules Rework

---
+++ src/components/Bubbles.tsx
@@ -0,0 +1,60 @@
+import "../styles/Bubbles.scss";
+
+const Bubbles: React.FC = () => {
+  return (
+    <div className="bubble-container">
+      <div className="bubble"></div>
+      <div className="bubble"></div>
+      <div className="bubble"></div>
+      <div className="bubble"></div>
+      <div className="bubble"></div>
+      <div className="bubble"></div>
+      <div className="bubble"></div>
+      <div className="bubble"></div>
+      <div className="bubble"></div>
+      <div className="bubble"></div>
+      <div className="bubble"></div>
+      <div className="bubble"></div>
+      <div className="bubble"></div>
+      <div className="bubble"></div>
+      <div className="bubble"></div>
+      <div className="bubble"></div>
+      <div className="bubble"></div>
+      <div className="bubble"></div>
+      <div className="bubble"></div>
+      <div className="bubble"></div>
+      <div className="bubble"></div>
+      <div className="bubble"></div>
+      <div className="bubble"></div>
+      <div className="bubble"></div>
+      <div className="bubble"></div>
+      <div className="bubble"></div>
+      <div className="bubble"></div>
+      <div className="bubble"></div>
+      <div className="bubble"></div>
+      <div className="bubble"></div>
+      <div className="bubble"></div>
+      <div className="bubble"></div>
+      <div className="bubble"></div>
+      <div className="bubble"></div>
+      <div className="bubble"></div>
+      <div className="bubble"></div>
+      <div className="bubble"></div>
+      <div className="bubble"></div>
+      <div className="bubble"></div>
+      <div className="bubble"></div>
+      <div className="bubble"></div>
+      <div className="bubble"></div>
+      <div className="bubble"></div>
+      <div className="bubble"></div>
+      <div className="bubble"></div>
+      <div className="bubble"></div>
+      <div className="bubble"></div>
+      <div className="bubble"></div>
+      <div className="bubble"></div>
+      <div className="bubble"></div>
+    </div>
+  );
+};
+
+export default Bubbles;
--- src/views/LandingPage.tsx
+++ src/views/LandingPage.tsx
@@ -1,23 +1,14 @@
 import "../styles/LandingPage.scss";
-// import Img_Man from "../media/Man.png";
-import Img_Mike from "../media/Mike2022ImageGray.png";
-import HomeIcon from "@mui/icons-material/Home";
-import {
-  FaReact,
-  FaJs,
-  FaHtml5,
-  FaCss3Alt,
-  FaGithub,
-  FaGitAlt,
-} from "react-icons/fa";
+import Jumbotron from "../components/Jumbotron";
+import Bubbles from "../components/Bubbles";
 
-const LandingPage = () => {
+const LandingPage: React.FC = () => {
   return (
     <div>
       {/* <div className="sec-1"></div> */}
 
       <div className="landing-page">
-        <div className="bubbles">
+        <div className="screen">
           <div className="sec-1">
             {/* <div className="technologies">🧐testtest</div> */}
             {/* <div className="jumbotron">
@@ -47,147 +38,9 @@ const LandingPage = () => {
               </div>
             </div> */}
 
-            <div className="jumbotron">
-              <div className="container">
-                <div className="row">
-                  <div className="col-sm center-image">
-                    <img
-                      src={Img_Mike}
-                      className="img-fluid my-image"
-                      alt="Responsive image"
-                    ></img>
-                  </div>
-                  <div className="col-sm jumbotron__description">
-                    <p className="jumbotron__description--sec1">
-                      Michael Bateman
-                    </p>
-                    <p className="jumbotron__description--sec2">
-                      {`React & React Native Developer`}
-                    </p>
-                    <p className="jumbotron__description--icons">
-                      <FaReact
-                        style={{
-                          color: "#61DBFB",
-                          backgroundColor: "#323330",
-                          borderRadius: 5,
-                          borderStyle: "solid",
-                          borderWidth: 1,
-                        }}
-                      />{" "}
-                      <FaHtml5
-                        style={{
-                          color: "#e34c26",
-                          backgroundColor: "#ebebeb",
-                          borderRadius: 5,
-                          borderStyle: "solid",
-                          borderWidth: 1,
-                        }}
-                      />{" "}
-                      <FaCss3Alt
-                        style={{
-                          color: "#264de4",
-                          backgroundColor: "#ebebeb",
-                          borderRadius: 5,
-                          borderStyle: "solid",
-                          borderWidth: 1,
-                          borderColor: "#264de4",
-                        }}
-                      />{" "}
-                      <FaJs
-                        style={{
-                          color: "#F0DB4F",
-                          backgroundColor: "#323330",
-                          borderRadius: 5,
-                          borderStyle: "solid",
-                          borderWidth: 1,
-                          borderColor: "#323330",
-                        }}
-                      />{" "}
-                      <FaGithub
-                        style={{
-                          color: "white",
-                          backgroundColor: "#323330",
-                          borderRadius: 5,
-                          borderStyle: "solid",
-                          borderWidth: 1,
-                          borderColor: "#323330",
-                        }}
-                      />{" "}
-                      <FaGitAlt
-                        style={{
-                          color: "#F1502F",
-                          backgroundColor: "white",
-                          borderRadius: 5,
-                          borderStyle: "solid",
-                          borderWidth: 1,
-                          borderColor: "#3E2C00",
-                        }}
-                      />
-                    </p>
-                    <p className="jumbotron__description--sec3">
-                      {`I'm a Software Engineering, specialized in FrontEnd
-                      Development (React & React Native).`}
-                    </p>
-                    <p className="jumbotron__description--sec3">
-                      I'm always happy and willing to learn any technology
-                      that's needed for a project.
-                    </p>
-                  </div>
-                </div>
-              </div>
-            </div>
+            <Jumbotron />
           </div>
-
-          <div className="bubble"></div>
-          <div className="bubble"></div>
-          <div className="bubble"></div>
-          <div className="bubble"></div>
-          <div className="bubble"></div>
-          <div className="bubble"></div>
-          <div className="bubble"></div>
-          <div className="bubble"></div>
-          <div className="bubble"></div>
-          <div className="bubble"></div>
-          <div className="bubble"></div>
-          <div className="bubble"></div>
-          <div className="bubble"></div>
-          <div className="bubble"></div>
-          <div className="bubble"></div>
-          <div className="bubble"></div>
-          <div className="bubble"></div>
-          <div className="bubble"></div>
-          <div className="bubble"></div>
-          <div className="bubble"></div>
-          <div className="bubble"></div>
-          <div className="bubble"></div>
-          <div className="bubble"></div>
-          <div className="bubble"></div>
-          <div className="bubble"></div>
-          <div className="bubble"></div>
-          <div className="bubble"></div>
-          <div className="bubble"></div>
-          <div className="bubble"></div>
-          <div className="bubble"></div>
-          <div className="bubble"></div>
-          <div className="bubble"></div>
-          <div className="bubble"></div>
-          <div className="bubble"></div>
-          <div className="bubble"></div>
-          <div className="bubble"></div>
-          <div className="bubble"></div>
-          <div className="bubble"></div>
-          <div className="bubble"></div>
-          <div className="bubble"></div>
-          <div className="bubble"></div>
-          <div className="bubble"></div>
-          <div className="bubble"></div>
-          <div className="bubble"></div>
-          <div className="bubble"></div>
-          <div className="bubble"></div>
-          <div className="bubble"></div>
-          <div className="bubble"></div>
-          <div className="bubble"></div>
-          <div className="bubble"></div>
+          <Bubbles />
         </div>
       </div>
     </div>
