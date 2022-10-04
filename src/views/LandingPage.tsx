afd313c05f906105c1a0df333a6146f0db19dbaa
MikeNotu <michael.bateman@globant.com>
2022-10-04 16:23:01 -0500

    Jumbotron Finished

--- src/views/LandingPage.tsx
+++ src/views/LandingPage.tsx
@@ -1,6 +1,15 @@
 import "../styles/LandingPage.scss";
 // import Img_Man from "../media/Man.png";
 import Img_Mike from "../media/Mike2022ImageGray.png";
+import HomeIcon from "@mui/icons-material/Home";
+import {
+  FaReact,
+  FaJs,
+  FaHtml5,
+  FaCss3Alt,
+  FaGithub,
+  FaGitAlt,
+} from "react-icons/fa";
 
 const LandingPage = () => {
   return (
@@ -41,10 +50,10 @@ const LandingPage = () => {
             <div className="jumbotron">
               <div className="container">
                 <div className="row">
-                  <div className="col-sm">
+                  <div className="col-sm center-image">
                     <img
                       src={Img_Mike}
-                      className="img-fluid man"
+                      className="img-fluid my-image"
                       alt="Responsive image"
                     ></img>
                   </div>
@@ -52,7 +61,69 @@ const LandingPage = () => {
                     <p className="jumbotron__description--sec1">
                       Michael Bateman
                     </p>
-                    <p className="jumbotron__description--sec2">{`React & React Native Developer`}</p>
+                    <p className="jumbotron__description--sec2">
+                      {`React & React Native Developer`}
+                    </p>
+                    <p className="jumbotron__description--icons">
+                      <FaReact
+                        style={{
+                          color: "#61DBFB",
+                          backgroundColor: "#323330",
+                          borderRadius: 5,
+                          borderStyle: "solid",
+                          borderWidth: 1,
+                        }}
+                      />{" "}
+                      <FaHtml5
+                        style={{
+                          color: "#e34c26",
+                          backgroundColor: "#ebebeb",
+                          borderRadius: 5,
+                          borderStyle: "solid",
+                          borderWidth: 1,
+                        }}
+                      />{" "}
+                      <FaCss3Alt
+                        style={{
+                          color: "#264de4",
+                          backgroundColor: "#ebebeb",
+                          borderRadius: 5,
+                          borderStyle: "solid",
+                          borderWidth: 1,
+                          borderColor: "#264de4",
+                        }}
+                      />{" "}
+                      <FaJs
+                        style={{
+                          color: "#F0DB4F",
+                          backgroundColor: "#323330",
+                          borderRadius: 5,
+                          borderStyle: "solid",
+                          borderWidth: 1,
+                          borderColor: "#323330",
+                        }}
+                      />{" "}
+                      <FaGithub
+                        style={{
+                          color: "white",
+                          backgroundColor: "#323330",
+                          borderRadius: 5,
+                          borderStyle: "solid",
+                          borderWidth: 1,
+                          borderColor: "#323330",
+                        }}
+                      />{" "}
+                      <FaGitAlt
+                        style={{
+                          color: "#F1502F",
+                          backgroundColor: "white",
+                          borderRadius: 5,
+                          borderStyle: "solid",
+                          borderWidth: 1,
+                          borderColor: "#3E2C00",
+                        }}
+                      />
+                    </p>
                     <p className="jumbotron__description--sec3">
                       {`I'm a Software Engineering, specialized in FrontEnd
                       Development (React & React Native).`}
