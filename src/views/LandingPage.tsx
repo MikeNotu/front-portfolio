5c683476c16fa5594334f6e0001cd49c8d1328ce
MikeNotu <michael.bateman@globant.com>
2022-10-07 16:58:13 -0500

    Technologies wip

--- src/views/LandingPage.tsx
+++ src/views/LandingPage.tsx
@@ -10,34 +10,13 @@ const LandingPage: React.FC = () => {
       <div className="landing-page">
         <div className="screen">
           <div className="sec-1">
-            {/* <div className="technologies">🧐testtest</div> */}
-            {/* <div className="jumbotron">
-              <img
-                src={Img_Man}
-                className="img-fluid man"
-                alt="Responsive image"
-              ></img>
-              <div className="container-fluid">
-                <p className="jumbotron__description--sec1">Michael Bateman</p>
-                <p className="jumbotron__description--sec2">{`React & React Native Developer`}</p>
-                <p className="jumbotron__description--sec3">
-                  I'm a Software Engineering, specialized in FrontEnd
-                  Development.
-                </p>
-                <p className="jumbotron__description--sec3">
-                  I'm always happy and willing to learn any technology that's
-                  needed for a project.
-                </p>
+            <div className="technologies">
+              <div className="technologies__description">
+                <p className="technologies__description--sec1">test</p>
+                <p>test</p>
+                <p>test</p>
               </div>
-            </div> */}
-
-            {/* <div className="container">
-              <div className="column">
-                <div className="col-sm">One of three columns</div>
-                <div className="col-sm">One of three columns</div>
-              </div>
-            </div> */}
-
+            </div>
             <Jumbotron />
           </div>
           <Bubbles />
